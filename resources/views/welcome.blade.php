c635bdb4893e0157713849d96396fb624841b9da
Salesio  Kiur <salesio.kiura@gmail.com>
2023-10-11 16:06:09 +0300

    HomePage text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,18 +12,24 @@
         <div class="container position-relative">
             <div class="row gy-5" data-aos="fade-in">
                 <div class="col-lg-7 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start">
-                    <h2>Get Interns and Employees to fill any position you have. </h2>
-                    <p>Get the best talent from a pull of top institutions of higher learning in Kenya. This platform
-                        enables your organization to get aplications from talented students for any internship or employment
-                        position and select the most suitable talent. </p>
-                    {{-- <p>Get the best talent from a pull of top institutions of higher learning in Kenya. This platform
-                        enables your organization to get aplications from talented students for any internship or employment
-                        position and select the most suitable talent. </p> --}}
+                    <h2>Career resources are top priority for your members, so don’t hold back. </h2>
+                    <p>Transform your job board into a career center with the career development resources your members
+                        expect. Members look to their associations as a trusted place to help them on their career journey.
+                        Offer them career resources for every step along the way:
+                        Boost member engagement with relevant and valuable career articles that speak directly to your
+                        members.
+                        Deliver additional member value with industry-specific career coaches.
+                        Increase retention with career paths to help members plan their next career step.
+                        Build up your members by offering one centralized location for resume/CV review, LinkedIn profile
+                        makeover, and interview prep.
+                        Learn how to provide the career benefits your members have wanted all along.
+                    </p>
 
-                        <div class="d-flex justify-content-center justify-content-lg-start">
 
-                            <a href="{{ route('getstarted') }}" class="btn-get-started">Get Started</a>
-                        </div>
+                    <div class="d-flex justify-content-center justify-content-lg-start">
+
+                        <a href="{{ route('getstarted') }}" class="btn-get-started">Get Started</a>
+                    </div>
                 </div>
                 <div class="col-lg-5 order-1 order-lg-2">
                     <img src="{{ asset('assets/img/heroimg.png') }}" class="img-fluid" alt="" data-aos="zoom-out"
@@ -38,9 +44,11 @@
 
                     <div class="col-lg-4 col-md-6" id="intro-divs" data-aos="fade-up" data-aos-delay="100">
                         <div class="icon-box">
-                            <h4 class="title"><a href="{{ route('employer.create') }}" class="stretched-link">Corporate</a> </h4>
+                            <h4 class="title"><a href="{{ route('employer.create') }}" class="stretched-link">Corporate</a>
+                            </h4>
 
-                            <p class="text">You have internship, employment position your want to get the best employee?</p>
+                            <p class="text">You have internship, employment position your want to get the best employee?
+                            </p>
 
                             {{-- <p class="text">Do you have any internship / employment vacancy and you are seeking students with skill and talent to fill those
                                 position?</p> --}}
@@ -54,7 +62,8 @@
                     <div class="col-lg-4 col-md-6" id="intro-divs" data-aos="fade-up" data-aos-delay="200">
                         <div class="icon-box">
                             <h4 class="title"><a href="" class="stretched-link">University / College</a></h4>
-                            <p class="text">Get first notified of opportunities when they are available so you can connect your students. </p>
+                            <p class="text">Get first notified of opportunities when they are available so you can connect
+                                your students. </p>
                             <a href="{{ route('college.create') }}">Get notified of
                                 openings <i class="fa-solid fa-arrow-right-long"></i></a>
 
@@ -64,7 +73,8 @@
                     <div class="col-lg-4 col-md-6" id="intro-divs" data-aos="fade-up" data-aos-delay="300">
                         <div class="icon-box">
                             <h4 class="title"><a href="" class="stretched-link">Students</a></h4>
-                            <p>Access internship and employment opporunities from leading companies in Kenya and Beyond. </p>
+                            <p>Access internship and employment opporunities from leading companies in Kenya and Beyond.
+                            </p>
                             {{-- <p class="text">Are you pursuing undergraduate/post-graduate degree or diploma and would like to get access
                                 to internship/employment opportunities at a top institution? </p> --}}
                             <a href="{{ route('student.create') }}">Register to apply <i
@@ -121,7 +131,8 @@
                     <div class="col-lg-4 col-md-6">
                         <div class="service-item  position-relative">
                             <h3>Internship Advertisement</h3>
-                            <p>We provide a a plartform to get the best students for intership opportunities in your institution. You can access hundreds of applicants and sieve the best fit for you. </p>
+                            <p>We provide a a plartform to get the best students for intership opportunities in your
+                                institution. You can access hundreds of applicants and sieve the best fit for you. </p>
                             <a href="#" class="readmore stretched-link">Read more <i
                                     class="bi bi-arrow-right"></i></a>
                         </div>
@@ -131,7 +142,9 @@
                         <div class="service-item position-relative">
 
                             <h3>Job Advertisement</h3>
-                            <p>Hire recent undergraduates, graduates, masters and doctorate students conveniently by advertising you job on our platform and choose the most qualified employee for your organization </p>
+                            <p>Hire recent undergraduates, graduates, masters and doctorate students conveniently by
+                                advertising you job on our platform and choose the most qualified employee for your
+                                organization </p>
                             <a href="#" class="readmore stretched-link">Read more <i
                                     class="bi bi-arrow-right"></i></a>
                         </div>
@@ -140,7 +153,8 @@
                     <div class="col-lg-4 col-md-6">
                         <div class="service-item position-relative">
                             <h3>Access to opportunities</h3>
-                            <p>Students and graduates can access, apply and get job opportunities as soon as they are advertised on our platform. </p>
+                            <p>Students and graduates can access, apply and get job opportunities as soon as they are
+                                advertised on our platform. </p>
                             <a href="#" class="readmore stretched-link">Read more <i
                                     class="bi bi-arrow-right"></i></a>
                         </div>
@@ -149,7 +163,8 @@
                     <div class="col-lg-4 col-md-6">
                         <div class="service-item position-relative">
                             <h3>Institutions / Universities </h3>
-                            <p>Institutions can connect their students to opportunities in various industries through this robust platform. They can receive</p>
+                            <p>Institutions can connect their students to opportunities in various industries through this
+                                robust platform. They can receive</p>
                             <a href="#" class="readmore stretched-link">Read more <i
                                     class="bi bi-arrow-right"></i></a>
                         </div>
